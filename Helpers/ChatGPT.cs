b317f4a7e3f297d5aeba28504699530178e8bd30
Alan Tanner <alan@alanctanner.com>
2023-03-11 16:51:19 -0500

    Add project files.

---
+++ Helpers/ChatGPT.cs
@@ -0,0 +1,85 @@
+﻿
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ChatGPT_.NET_CLI.Helpers
+{
+    internal class ChatGPT
+    {
+        public static async Task RunChat(IOpenAIService sdk, string systemPrompt)
+        {
+            Console.WriteLine("Chat Completion starting:");
+            try
+            {
+                var history = new List<ChatMessage>
+                {
+                    new(StaticValues.ChatMessageRoles.System, systemPrompt)
+                };
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    var completionResult = sdk.ChatCompletion.CreateCompletionAsStream(new ChatCompletionCreateRequest
+                    {
+                        Messages = history,
+                        Model = Models.ChatGpt3_5Turbo
+                    });
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    await foreach (var completion in completionResult)
+                    {
+                        if (completion.Successful)
+                        {
+                            string reply = completion.Choices.First().Message.Content;
+                            Console.Write(reply);
+                            stringBuilder.Append(reply);
+                        }
+                        else
+                        {
+                            if (completion.Error == null)
+                            {
+                                throw new Exception("Unknown Error");
+                            }
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{completion.Error.Code}: {completion.Error.Message}");
+                            break;
+                        }
+                    }
+                    Console.ResetColor();
+                    Console.Write("\n\n");
+
+                    //save response from chatGPT
+                    history.Add(new(StaticValues.ChatMessageRoles.Assistant, stringBuilder.ToString()));
+                    
+                    string? message = string.Empty;
+                    do
+                    {
+                        message = Console.ReadLine();
+                    } while (string.IsNullOrEmpty(message));
+                    Console.Write('\n');
+                    history.Add(new(StaticValues.ChatMessageRoles.User, message));
+
+                    //if user types exit, end the chat
+                    if (string.Equals(message, "exit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ChatGPT_.NET_CLI.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
+
+var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+IConfiguration configuration = builder.Build();
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped(_ => configuration);
+
+serviceCollection.AddOpenAIService();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
+
+//this message is the starting prompt for chatGPT
+string systemMessage = "Introduce yourself";
+
+await ChatGPT.RunChat(sdk, systemMessage);
