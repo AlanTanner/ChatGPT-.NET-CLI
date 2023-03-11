490a3af52b7a0f0239f4800640ed9b72739fdf9c
Alan Tanner <alan@alanctanner.com>
2023-03-11 17:14:24 -0500

    added try catch to display error for missing api key

--- Program.cs
+++ Program.cs
@@ -4,18 +4,27 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenAI.GPT3.Extensions;
 using OpenAI.GPT3.Interfaces;
 
-var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
-IConfiguration configuration = builder.Build();
+try
+{
+    var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+    IConfiguration configuration = builder.Build();
 
-var serviceCollection = new ServiceCollection();
-serviceCollection.AddScoped(_ => configuration);
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddScoped(_ => configuration);
 
-serviceCollection.AddOpenAIService();
+    serviceCollection.AddOpenAIService();
 
-var serviceProvider = serviceCollection.BuildServiceProvider();
-var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+    var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
 
-//this message is the starting prompt for chatGPT
-string systemMessage = "Introduce yourself";
+    //this message is the starting prompt for chatGPT
+    string systemMessage = "Introduce yourself";
 
-await ChatGPT.RunChat(sdk, systemMessage);
+    await ChatGPT.RunChat(sdk, systemMessage);
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ex.Message);
+    Console.ResetColor();
+}
